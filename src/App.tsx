75dc76728045863d9e1e3e9bff896c593f1ac656
JackiesNguyen <hainguyen.061001@gmail.com>
2024-11-07 01:11:19 +0700

    refa: change title times

--- src/App.tsx
+++ src/App.tsx
@@ -28,18 +28,50 @@ const App = () => {
   );
   const [isGameOver, setIsGameOver] = useState<boolean>(false);
   const [isAutoPlay, setIsAutoPlay] = useState<boolean>(false);
-  const autoPlayIntervalRef = useRef<NodeJS.Timeout | null>(null); // Ref để lưu trữ interval của Auto Play
-  const intervalsRef = useRef<NodeJS.Timeout[]>([]); // Mảng để lưu trữ tất cả các interval
+  const autoPlayIntervalRef = useRef<NodeJS.Timeout | null>(null);
+  const intervalsRef = useRef<NodeJS.Timeout[]>([]);
+
+  const getRandomPosition = () => {
+    const randomX = Math.random() * 90;
+    const randomY = Math.random() * 90;
+    return {
+      left: `${randomX}%`,
+      top: `${randomY}%`,
+    };
+  };
+
+  const generateButtons = (count: number) => {
+    return Array.from({ length: count }).map((_, index) => ({
+      key: uuidv4(),
+      index,
+      position: getRandomPosition(),
+      counter: 3,
+      opacity: 1,
+    }));
+  };
+
+  const startTimer = () => {
+    if (intervalId) {
+      clearInterval(intervalId);
+    }
+
+    const id = setInterval(() => {
+      setCounter((prevCounter) => prevCounter + 0.1);
+    }, 100);
+
+    setIntervalId(id);
+    setTimeout(() => clearInterval(id), 60000);
+  };
 
   const handleButtonClick = useCallback(
     (buttonIndex: number) => {
       const button = buttons[buttonIndex];
-      if (button.opacity < 0.01) return; // Bỏ qua các nút có opacity gần bằng 0
+      if (button.opacity < 0.01) return;
 
       const updatedButtons = [...buttons];
-      updatedButtons[buttonIndex].counter = 3; // Reset lại bộ đếm mỗi lần nút được nhấn
-      updatedButtons[buttonIndex].opacity = 1; // Đặt opacity về 1 khi bắt đầu lại
-      setButtons(updatedButtons); // Cập nhật lại buttons
+      updatedButtons[buttonIndex].counter = 3;
+      updatedButtons[buttonIndex].opacity = 1;
+      setButtons(updatedButtons);
 
       if (buttonIndex + 1 === nextButtonIndex) {
         setClickedButtons((prev) => new Set(prev).add(buttonIndex));
@@ -54,12 +86,12 @@ const App = () => {
           const button = updatedButtons[buttonIndex];
 
           if (button.counter > 0) {
-            button.counter -= 0.1; // Giảm bộ đếm
-            button.opacity = button.counter / 3; // Cập nhật opacity từ 1 -> 0
+            button.counter -= 0.1;
+            button.opacity = button.counter / 3;
 
-            setButtons(updatedButtons); // Cập nhật trạng thái buttons
-            setButtonCounters(
-              (prev) => new Map(prev).set(buttonIndex, button.counter) // Cập nhật bộ đếm vào state
+            setButtons(updatedButtons);
+            setButtonCounters((prev) =>
+              new Map(prev).set(buttonIndex, button.counter)
             );
           } else if (nextButtonIndex === buttons.length) {
             setGameStatus("ALL CLEARED");
@@ -69,24 +101,21 @@ const App = () => {
           }
         }, 100);
 
-        intervalsRef.current.push(interval); // Lưu interval vào mảng
-
+        intervalsRef.current.push(interval);
         setNextButtonIndex(nextButtonIndex + 1);
       } else {
         setGameStatus("GAME OVER");
         setIsGameOver(true);
-        setClickedButtons((prev) => new Set(prev).add(buttonIndex)); // Đảm bảo nút cuối cùng được nhấn có màu nền
-        setButtonCounters((prev) => new Map(prev).set(buttonIndex, 3)); // Đặt lại bộ đếm của nút cuối cùng được nhấn thành 3.0s
+        setClickedButtons((prev) => new Set(prev).add(buttonIndex));
+        setButtonCounters((prev) => new Map(prev).set(buttonIndex, 3));
         if (intervalId) clearInterval(intervalId);
-        // Clear tất cả các interval khi game over
         intervalsRef.current.forEach(clearInterval);
         intervalsRef.current = [];
-        // Dừng tất cả các nút
         setButtons((prevButtons) =>
           prevButtons.map((button) => ({
             ...button,
-            counter: button.counter, // Giữ nguyên giá trị counter
-            opacity: button.counter / 3, // Giữ nguyên giá trị opacity tương ứng với counter
+            counter: button.counter,
+            opacity: button.counter / 3,
           }))
         );
       }
@@ -98,17 +127,15 @@ const App = () => {
     if (isAutoPlay && isPlayGame && !isGameOver) {
       autoPlayIntervalRef.current = setInterval(() => {
         handleButtonClick(nextButtonIndex - 1);
-      }, 1000);
+      }, 100);
 
       return () => {
         if (autoPlayIntervalRef.current) {
           clearInterval(autoPlayIntervalRef.current);
         }
       };
-    } else {
-      if (autoPlayIntervalRef.current) {
-        clearInterval(autoPlayIntervalRef.current);
-      }
+    } else if (autoPlayIntervalRef.current) {
+      clearInterval(autoPlayIntervalRef.current);
     }
   }, [isAutoPlay, isPlayGame, isGameOver, nextButtonIndex, handleButtonClick]);
 
@@ -135,83 +162,40 @@ const App = () => {
     setIsAutoPlay((prev) => !prev);
   };
 
-  const getRandomPosition = () => {
-    const randomX = Math.random() * 90;
-    const randomY = Math.random() * 90;
-    return {
-      left: `${randomX}%`,
-      top: `${randomY}%`,
-    };
-  };
-
-  const generateButtons = (count: number) => {
-    return Array.from({ length: count }).map((_, index) => {
-      const position = getRandomPosition();
-      const key = uuidv4();
-      return {
-        key,
-        index,
-        position,
-        counter: 3, // Bắt đầu với bộ đếm 3s
-        opacity: 1, // Độ mờ ban đầu là 1 (đầy đủ)
-      };
-    });
-  };
-
-  const startTimer = () => {
-    // Clear interval nếu đang chạy
-    if (intervalId) {
-      clearInterval(intervalId);
-    }
-
-    const id = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 0.1);
-    }, 100);
-
-    setIntervalId(id); // Lưu intervalId vào state
-    setTimeout(() => clearInterval(id), 60000); // Dừng sau 60 giây
-  };
-
   const resetGame = () => {
-    // Clear tất cả các interval
     intervalsRef.current.forEach(clearInterval);
     intervalsRef.current = [];
 
-    // Clear interval nếu đang chạy
     if (intervalId) {
       clearInterval(intervalId);
-      setIntervalId(null); // Đảm bảo rằng intervalId được reset
+      setIntervalId(null);
     }
 
-    // Clear auto play interval nếu đang chạy
     if (autoPlayIntervalRef.current) {
       clearInterval(autoPlayIntervalRef.current);
       autoPlayIntervalRef.current = null;
     }
 
-    // Reset lại trạng thái của buttons (bao gồm counter và opacity)
     setButtons((prevButtons) =>
       prevButtons.map((button) => ({
         ...button,
-        counter: 3, // Thiết lập lại bộ đếm (hoặc giá trị mặc định)
-        opacity: 1, // Đặt opacity về 1 (hoặc giá trị mặc định)
+        counter: 3,
+        opacity: 1,
       }))
     );
 
-    // Reset các trạng thái game
     setGameStatus("LET'S PLAY");
     setNextButtonIndex(1);
     setClickedButtons(new Set());
     setButtonCounters(new Map());
     setCounter(0);
     setIsGameOver(false);
-    setIsAutoPlay(false); // Tắt chế độ Auto Play khi reset
+    setIsAutoPlay(false);
 
-    // Tạo lại buttons sau khi reset
     setTimeout(() => {
-      setButtons(generateButtons(Number(pointsCount))); // Tạo lại buttons mới
-      startTimer(); // Khởi động lại bộ đếm thời gian
-    }, 0); // Đảm bảo việc render lại buttons là tức thời
+      setButtons(generateButtons(Number(pointsCount)));
+      startTimer();
+    }, 0);
   };
 
   const getStatusColor = () => {
@@ -243,7 +227,7 @@ const App = () => {
         />
       </div>
       <div className="flex items-center gap-5">
-        <Label htmlFor="time">BỘ ĐẾM TỔNG:</Label>
+        <Label htmlFor="time">Times:</Label>
         <div className="px-2">{counter.toFixed(1)}s</div>
       </div>
       <div className="flex items-center gap-5">
@@ -265,18 +249,18 @@ const App = () => {
         {isPlayGame &&
           buttons.map(({ key, index, position, opacity }) => (
             <Button
-              key={key} // Gán key duy nhất cho mỗi button
+              key={key}
               className={`rounded-full w-12 h-12 ${
                 clickedButtons.has(index) ? "bg-orange-500 z-10" : "bg-white"
               } border-2 border-orange-500 hover:bg-orange-500 ${
                 clickedButtons.has(index) && !isGameOver ? "fade-out" : ""
-              }`} // Loại bỏ lớp fade-out khi game over
+              }`}
               style={{
                 position: "absolute",
                 left: position.left,
                 top: position.top,
-                opacity: opacity, // Giữ nguyên opacity tương ứng với counter
-                pointerEvents: opacity < 0.01 ? "none" : "auto", // Vô hiệu hóa sự kiện chuột khi opacity gần bằng 0
+                opacity: opacity,
+                pointerEvents: opacity < 0.01 ? "none" : "auto",
               }}
               onClick={() => handleButtonClick(index)}
             >
